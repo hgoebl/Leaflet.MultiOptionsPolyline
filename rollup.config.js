5c7e21fee827af32fc246e030938dd1834eac864
Luc Moell <luc.moell@gmail.com>
2018-03-08 23:05:14 +0100

    umd addition

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,16 @@ import typescript from 'rollup-plugin-typescript';
 
 export default {
   input: './src/MultiOptionsPolyline.ts',
-  output: {
-    file: 'dist/MultiOptionsPolyline.js',
-    format: 'iife'
-  },
+  output: [
+    {
+      file: 'dist/MultiOptionsPolyline.js',
+      format: 'iife'
+    },
+    {
+      file: 'dist/MultiOptionsPolyline.umd.js',
+      format: 'umd'
+    }
+  ],
   name: 'MultiOptionsPolyline',
   globals: {
     leaflet: 'L'
